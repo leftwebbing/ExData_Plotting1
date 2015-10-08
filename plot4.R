2f4a5f3951a95fa33ecd964b964f5def2142eebe
leftwebbing <leftwebbing@users.noreply.github.com>
2015-10-08 19:48:42 -0400

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,9 @@
++# generates plot4.png
++
++#read file into data frame
++
+multi plot
+global active power vs weekday
+voltage vs weekday
+energy sub metering vs weekday [from plot 3]
+global reactive power vs weekday
